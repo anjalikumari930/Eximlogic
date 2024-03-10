1e1b39b7cb28359a5d8d29ac1c17c2abfd925dfd
anjalikumari930 <anjalikumari93045@gmail.com>
2024-03-10 14:55:59 +0530

    initial

---
+++ server/controller/authcontroller.js
@@ -0,0 +1,195 @@
+import user from "../models/user.js";
+import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
+
+export const registerController = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Validation
+    if (!username || !email || !password) {
+      return res.status(400).send({
+        success: false,
+        message: "Please provide username, email, and password",
+      });
+    }
+
+    // Check if the username or email is already registered
+    const existingUser = await user.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(400).send({
+        success: false,
+        message: "Username or email already registered",
+      });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    // Create a new user
+    const newUser = new user({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    res.status(201).send({
+      success: true,
+      message: "User registered successfully",
+      user: {
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in registration",
+      error,
+    });
+  }
+};
+
+//POST LOGIN
+export const loginController = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    //validation
+    if (!username || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid username or password",
+      });
+    }
+    //check user
+    const employee = await user.findOne({ username });
+    if (!employee) {
+      return res.status(404).send({
+        success: false,
+        message: "Username is not registered",
+      });
+    }
+    const match = await comparePassword(password, employee.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+    //token
+    const token = await JWT.sign(
+      { _id: employee._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Login successfully",
+      user: {
+        _id: employee._id,
+        username: employee.username,
+        email: employee.email,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
+      error,
+    });
+  }
+};
+
+//forgotPasswordController
+
+export const forgotPasswordController = async (req, res) => {
+  try {
+    const { email, newPassword } = req.body;
+    if (!email) {
+      res.status(400).send({ message: "Email is required" });
+    }
+    if (!newPassword) {
+      res.status(400).send({ message: "New Password is required" });
+    }
+    //check
+    const employee = await user.findOne({ email });
+    //validation
+    if (!employee) {
+      return res.status(404).send({
+        success: false,
+        message: "Wrong Email Or Answer",
+      });
+    }
+    const hashed = await hashPassword(newPassword);
+    await user.findByIdAndUpdate(employee._id, { password: hashed });
+    res.status(200).send({
+      success: true,
+      message: "Password Reset Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
+  }
+};
+
+//test controller
+export const testController = (req, res) => {
+  try {
+    res.send("Protected Routes");
+  } catch (error) {
+    console.log(error);
+    res.send({ error });
+  }
+};
+
+export const updateProfileController = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Retrieve the user from the database
+    const existingUser = await user.findById(req.user._id);
+    if (!existingUser) {
+      return res.status(404).send({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Update the user properties if provided in the request body
+    if (username) {
+      existingUser.username = username;
+    }
+    if (email) {
+      existingUser.email = email;
+    }
+    if (password) {
+      existingUser.password = password;
+    }
+
+    // Save the updated user
+    const updatedUser = await existingUser.save();
+
+    res.status(200).send({
+      success: true,
+      message: "Profile Updated Successfully",
+      updatedUser,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      success: false,
+      message: "Error While Updating Profile",
+      error,
+    });
+  }
+};
