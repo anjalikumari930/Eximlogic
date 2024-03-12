74253e67d2a6456b1b6055a54906b44503041d98
anandkr47 <anandkumar.trident@gmail.com>
2024-03-13 01:11:45 +0530

    fix dashboard Lines

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -12,6 +12,7 @@ const Dashboard = () => {
       <h2 className="text-3xl font-bold tracking-tight text-grey-900">
       " Hi, {userData.user.username} Welcome back 👋"
             </h2>
+            <hr className="my-2 border-gray-200" />
         <div className="flex flex-col md:flex-row md:space-x-4 sm:ml-20 ">
           <PieChart />
           <CustomerPieChart totalCustomers={150} />
